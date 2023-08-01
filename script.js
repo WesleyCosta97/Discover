635662822dd681d6e6038f5be18c34e3336b6eca
Wesley Costa <wesley.costa981@hotmail.com>
2023-08-01 19:08:30 -0300

    animation button

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-function toggleMode(){
-    const html = document.documentElement
-    
-    html.classList.toggle('light')
+function toggleMode() {
+  const html = document.documentElement
 
+  html.classList.toggle("light")
 
-    const img = document.querySelector("#profile img")
-   
-    if(html.classList.contains('light')){
-       img.setAttribute('src', './assets/wwhite.gif')  
-    } else{
-        img.setAttribute('src', './assets/black.gif')
-    }
-    
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/wwhite.gif")
+  } else {
+    img.setAttribute("src", "./assets/black.gif")
+  }
 }
